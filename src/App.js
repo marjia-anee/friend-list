7dc35ee01ab545233ce76087d8e09aa6f3aa5ece
marjia-anee <marjiazaman@gmail.com>
2020-08-28 11:06:24 +0600

    friend list is creating..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import User from './components/User/User';
 
 function App() {
+
+ const [users, setUsers] = useState([]);
+
+ 
+
+ useEffect(() => {
+
+  fetch ('https://randomuser.me/api/?results=15&fbclid=IwAR2Jb_myklf-xFoP10n9hVzF-xWuKZOWEMXFd5u0x-pM4LpbhD4BIGqJBmM')
+  .then (res => res.json ())
+  .then (data =>{ 
+    setUsers(data.results);
+   console.log(data.results);
+   const names = data.map (user => user.name)
+   console.log(names);
+  
+  } )
+  .catch (error => console.log(error))
+
+ }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>user loaded: {users.length}</h1>
+      <ul>
+      {
+        users.map(user => <User user={user}> </User>)
+      }
+      </ul>
     </div>
+    
   );
 }
 
---
+++ src/components/User/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = (props) => {
+    const = props.user;
+
+
+    return (
+        <div>
+            <h4>i am {name}</h4>
+        </div>
+    );
+};
+
+export default User;
